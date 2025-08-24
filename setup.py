b6f5d33b301a2a990e55ac61d8e04d1f72523b54
Aleksandr Dezhin <me@dezhin.net>
2025-08-24 21:44:08 +0300

    Install GDAL from PyPi for GDAL >= 3.7, PyGDAL otherwise
    NGW-1737

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 import os
-import sys
 import subprocess
-from os import path, makedirs
+import sys
+from os import makedirs, path
+from subprocess import CalledProcessError, check_output
 
-from setuptools import setup, find_packages, Extension
+from packaging.version import Version
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-
-with open('VERSION', 'r') as fd:
+with open("VERSION", "r") as fd:
     VERSION = fd.read().rstrip()
 
 
@@ -20,7 +21,6 @@ class CMakeExtension(Extension):
 
 
 class CMakeBuild(build_ext):
-
     def run(self):
         for ext in self.extensions:
             self.build_cmake(ext)
@@ -31,52 +31,58 @@ class CMakeBuild(build_ext):
 
         extdir = self.get_ext_fullpath(ext.name)
 
-        config = 'Debug' if self.debug else 'Release'
+        config = "Debug" if self.debug else "Release"
         cmake_args = [
-            '-DPYTHON_EXECUTABLE=' + sys.executable,
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + os.path.abspath(os.path.split(extdir)[0]),
-            '-DCMAKE_BUILD_TYPE=' + config,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY="
+            + os.path.abspath(os.path.split(extdir)[0]),
+            "-DCMAKE_BUILD_TYPE=" + config,
         ]
 
-        build_args = ['--config', config, '--', '-j2']
+        build_args = ["--config", config, "--", "-j2"]
 
         env = os.environ.copy()
         subprocess.check_call(
-            ['cmake', os.path.abspath(os.path.dirname(__file__))] + cmake_args,
-            cwd=self.build_temp, env=env)
+            ["cmake", os.path.abspath(os.path.dirname(__file__))] + cmake_args,
+            cwd=self.build_temp,
+            env=env,
+        )
 
         if not self.dry_run:
             subprocess.check_call(
-                ['cmake', '--build', '.'] + build_args,
-                cwd=self.build_temp, env=env)
+                ["cmake", "--build", "."] + build_args, cwd=self.build_temp, env=env
+            )
 
 
 try:
-    gv = subprocess.check_output(['gdal-config', '--version'], universal_newlines=True).strip()
-except subprocess.CalledProcessError:
-    gv = None
+    gv = check_output(["gdal-config", "--version"], universal_newlines=True).strip()
+    gdal_pkg = f"pygdal=={gv}.*" if Version(gv) < Version("3.7") else f"gdal=={gv}.*"
+except CalledProcessError:
+    gdal_pkg = "pygdal"
 
 
 setup(
-    name='terratile',
-    description='Simple wrapper of cesium terrain builder (libctb) for terrain tiles generation.',
+    name="terratile",
+    description="Simple wrapper of cesium terrain builder (libctb) for terrain tiles generation.",
     version=VERSION,
-    python_requires='>=3.8, <4',
-    url='https://github.com/nextgis/terratile',
-    author='NextGIS',
-    author_email='info@nextgis.com',
-    license='MIT',
-    packages=['terratile', ],
-    ext_modules=[CMakeExtension('_terratile')],
+    python_requires=">=3.8, <4",
+    url="https://github.com/nextgis/terratile",
+    author="NextGIS",
+    author_email="info@nextgis.com",
+    license="MIT",
+    packages=[
+        "terratile",
+    ],
+    ext_modules=[CMakeExtension("_terratile")],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
-    extras_require={'server': [
-        'fastapi',
-        'uvicorn',
-        'jinja2',
-        'pygdal' + (('==%s.*' % gv) if gv else ''),
-    ]},
-    package_data={
-        '': ['*.html', 'example/*']
+    extras_require={
+        "server": [
+            "fastapi",
+            "uvicorn",
+            "jinja2",
+            gdal_pkg,
+        ]
     },
+    package_data={"": ["*.html", "example/*"]},
 )
